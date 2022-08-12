0cad7187066c13b80d69f5b93c663f28c12d8718
eddiesmag <eddiesmag@gmail.com>
2022-08-12 15:17:51 +0300

    innital commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+//this is to load env vars to this config
+require('dotenv').config({ path: './.env' });
+// to pass env vars to the application
+const DotenvPlugin = require('webpack-dotenv-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000, // should be => process.env.PORT,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.(svg|png|jpeg|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+    new BundleAnalyzerPlugin(),
+    /**  from the DotenvPlugin documentation
+    new DotenvPlugin({
+      sample: './.env.default',
+      path: './.env',
+    }),*/
+  ],
+};
